504c01098f12bd59bab623c92c81b51225fca313
Sidra <smanzoor@andrew.cmu.edu>
2021-10-27 23:43:37 -0400

    New product listing on click + add selections to local storage

--- script.js
+++ script.js
@@ -25,10 +25,16 @@ if(cartBtnAvailable){
           quantitySelectedValue = rb2.value;
       }
   }
+
+
+//   adding selected radio value to local Storage. Need to do for loop and += to add all values 
     var item = [glazeSelectedValue, quantitySelectedValue];
+    localStorage.setItem("cartItems", item);
+    console.log(localStorage.getItem('cartItems'));
+    // console.log(cartItems);
     // items.push(item);
-    console.log("Glaze Selected Value = " + item[0]);
-    console.log("Quantity Selected Value = " + item[1]);
+    // console.log("Glaze Selected Value = " + item[0]);
+    // console.log("Quantity Selected Value = " + item[1]);
 //------------For 6B - Capturing radio button form selections-----------------------
 
 
@@ -81,26 +87,153 @@ document.body.addEventListener('change', function(e){
   switch (target.id) {
       case 'vanillaMilk':
           document.getElementById("rollimage").src = "assets/images/originaldetail.png";
-          message = 'vanillaMilk';
+          message = 'Vanilla Milk';
           break;
       case 'sugarMilk':
           document.getElementById("rollimage").src = "assets/images/walnut.png";
-          message = 'sugarMilk';
+          message = 'Sugar Milk';
           break;
       case 'doubleChocolate':
           document.getElementById("rollimage").src = "assets/images/blackberry.png";
-          message = 'doubleChocolate';
+          message = 'Double Chocolate';
           break;
       case 'noGlaze':
           document.getElementById("rollimage").src = "assets/images/noglaze.jpeg";
-          message = 'noGlaze';
+          message = 'No Glaze';
           break;
   }
   console.log(message);
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // update cart counter everytime a page is refreshed
 window.onload = updateCartOnAllPages();
+
+// ////Need to format everything here + use this create element on "add to cart" + window.onload
+var cartItemsContainer = document.getElementsByClassName("cartItems");
+
+
+    var productContainerDiv = document.createElement("div");
+
+    productContainerDiv.className = "cartItem marginItem";
+    // Product Image
+    var productImg = document.createElement("img");
+    productImg.className = "cartImage";
+    ///////// add src based on localStorage selected src
+    productImg.src = "assets/images/noglaze.jpeg";
+    //////////////////////////////////////////////////////
+
+
+    // Item Details Div
+    var itemDetailsDiv = document.createElement("div");
+    itemDetailsDiv.className = "cartItemDetails";
+
+    var itemTitleDiv = document.createElement("div");
+    itemTitleDiv.className = "itemTitle";
+    var title = document.createElement("p");
+    title.className = "cartItemName";
+    title.innerHTML = "Original";
+
+    var itemIconsDiv = document.createElement("div");
+    itemIconsDiv.className = "itemIcons";
+    var favoriteIcon = document.createElement("img");
+    favoriteIcon.src = "assets/icons/favorites.svg";
+    favoriteIcon.className = "iconlabels";
+    var deleteIcon = document.createElement("img");
+    deleteIcon.src = "assets/icons/delete.svg";
+    deleteIcon.className = "iconlabels";
+
+    var productPrice = document.createElement("h6");
+    productPrice.className = "itemPrice";
+    productPrice.innerHTML = "$1.99";
+
+    var dropdownsDiv = document.createElement("div");
+    dropdownsDiv.className = "cartItemDropdowns";
+    var quantityDropdownDiv = document.createElement("div");
+    quantityDropdownDiv.className = "dropdownSection firstDropdown";
+    var quantityTitleText = document.createElement("p");
+    quantityTitleText.className = "dropdownTitle";
+    quantityTitleText.innerHTML = "Quantity";
+    var quantityActualDropdownDiv = document.createElement("div");
+    quantityActualDropdownDiv.className = "dropdown";
+    var quantityDropdownText = document.createElement("p");
+    quantityDropdownText.className = "dropdownText";
+    // //////////////Change this based on items[1]
+    quantityDropdownText.innerHTML = "3";
+    // ///////////////////////////////////////////
+    var quantityDropdownArrow = document.createElement("img");
+    quantityDropdownArrow.className = "dropdownArrow";
+    quantityDropdownArrow.src = "assets/icons/arrow.svg";
+
+    var glazeDropdownDiv = document.createElement("div");
+    glazeDropdownDiv.className = "dropdownSection";
+    var glazeTitleText = document.createElement("p");
+    glazeTitleText.className = "dropdownTitle";
+    glazeTitleText.innerHTML= "Glaze";
+
+    var glazeActualDropdownDiv = document.createElement("div");
+    glazeActualDropdownDiv.className = "dropdown";
+    var glazeDropdownText = document.createElement("p");
+    glazeDropdownText.className = "dropdownText";
+    // /////////////////Change this based on items[0]
+    glazeDropdownText.innerHTML = "Vanilla-Milk";
+    ///////////////////////////////////////////
+    var glazeDropdownArrow = document.createElement("img");
+    glazeDropdownArrow.className = "dropdownArrow";
+    glazeDropdownArrow.src = "assets/icons/arrow.svg";
+
+
+glazeActualDropdownDiv.appendChild(glazeDropdownText);
+glazeActualDropdownDiv.appendChild(glazeDropdownArrow);
+glazeDropdownDiv.appendChild(glazeTitleText);
+glazeDropdownDiv.appendChild(glazeActualDropdownDiv);
+
+quantityActualDropdownDiv.appendChild(quantityDropdownText);
+quantityActualDropdownDiv.appendChild(quantityDropdownArrow);
+quantityDropdownDiv.appendChild(quantityTitleText);
+quantityDropdownDiv.appendChild(quantityActualDropdownDiv);
+
+dropdownsDiv.appendChild(quantityDropdownDiv);
+dropdownsDiv.appendChild(glazeDropdownDiv);
+
+
+itemTitleDiv.appendChild(title);
+itemIconsDiv.appendChild(favoriteIcon);
+itemIconsDiv.appendChild(deleteIcon);
+itemTitleDiv.appendChild(itemIconsDiv);
+itemDetailsDiv.appendChild(itemTitleDiv);
+
+
+itemDetailsDiv.appendChild(productPrice);
+itemDetailsDiv.appendChild(dropdownsDiv);
+
+productContainerDiv.appendChild(productImg);
+productContainerDiv.appendChild(itemDetailsDiv);
+
+
+
+var checkoutButton = document.getElementById('testBtn');
+if(checkoutButton){
+    checkoutButton.addEventListener("click", function(){
+        var cartItemsDiv = document.getElementById("cartDiv");
+        cartItemsDiv.appendChild(productContainerDiv);
+        console.log("New product added!");
+    });
+
+}
 
 
 
