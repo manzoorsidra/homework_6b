3f4c76c36abccff19019354bdc13256014164a7b
Sidra <smanzoor@andrew.cmu.edu>
2021-11-02 22:37:47 -0400

    Delete Item Instance from Cart

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Capture selected values of radio group and add to "items" array
 var myId;
 var items = [];
 
@@ -7,7 +6,7 @@ var cartBtnAvailable = document.getElementById('addToCartBtn');
 if(cartBtnAvailable){
   cartBtnAvailable.addEventListener('click', function(){
 
-//------------For 6B - Capturing radio button form selections-----------------------
+//Capture radio button form selections in variables
     const rbs = document.querySelectorAll('input[name="glaze_flavor"]');
     const rbs2 = document.querySelectorAll('input[name="quantity"]');
 
@@ -27,20 +26,14 @@ if(cartBtnAvailable){
   }
 
 
-//   adding selected radio value to local Storage. Need to do for loop and += to add all values 
+// Add selected radio button values to local Storage. Only adding first letter of glaze selection 
     var item = [`${glazeSelectedValue[0]}${quantitySelectedValue}`];
     items.push(item);
     localStorage.setItem("cartItems", items);
     console.log(localStorage.getItem('cartItems'));
 
-    // console.log(cartItems);
-    // items.push(item);
-    // console.log("Glaze Selected Value = " + item[0]);
-    // console.log("Quantity Selected Value = " + item[1]);
-//------------For 6B - Capturing radio button form selections-----------------------
 
-
-// Local Storage Cart Items Counter
+// Save Cart Items Counter to Local Storage
     currentCounterValue = parseInt(localStorage.getItem('cartItemNumber'));
 
     if (isNaN(currentCounterValue)){
@@ -51,13 +44,11 @@ if(cartBtnAvailable){
     var updatedCounter = currentCounterValue + 1;
     localStorage.setItem("cartItemNumber", JSON.stringify(updatedCounter));
 
-    document.getElementById("cartCounter").innerHTML = JSON.parse(localStorage.getItem('cartItemNumber'));
-
-    
+    document.getElementById("cartCounter").innerHTML = JSON.parse(localStorage.getItem('cartItemNumber')); 
   });
 };
 
-// Update cart items counter on all pages with the cart link
+// Update cart items counter on all pages that have the cart link
 function updateCartOnAllPages(){
 
     if(document.getElementById("cartCounter")){
@@ -79,17 +70,14 @@ function updateCartOnAllPages(){
         cartCount4.innerHTML = cartCountLS;
     }
 
-
-
-    let LSCartItems = localStorage.getItem('cartItems');
-    console.log(typeof(LSCartItems));
-    LSCartItems =LSCartItems.split(",");
-    console.log(LSCartItems);
-
-
-    LSCartItems.forEach(item => {
-        addProductToCart(item);
-    });
+// Add each selection detail to each product listing in the cart page
+if (localStorage.getItem('cartItems')){
+        let LSCartItems = localStorage.getItem('cartItems');
+        LSCartItems = LSCartItems.split(",");
+        LSCartItems.forEach(item => {
+            addProductToCart(item);
+        });
+    }
 };
 
 
@@ -116,64 +104,29 @@ document.body.addEventListener('change', function(e){
           message = 'No Glaze';
           break;
   }
-  console.log(message);
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// update cart counter everytime a page is refreshed
+// update cart counter + product listing on cart page everytime a page is refreshed
 window.onload = updateCartOnAllPages();
 
-// ////Need to format everything here + use this create element on "add to cart" + window.onload
 
-
-
-
-var checkoutButton = document.getElementById('testBtn');
-if(checkoutButton){
-    checkoutButton.addEventListener("click", function(){
-
-    });
-
-}
-
-
-
+// Create a product listing div, insert item type and item quantity from local storage
 function addProductToCart(item){
     let itemType = item[0];
-    // console.log(item.substring);
     let itemQty = item.substring(1);
-
 
     const itemTypes = {V: "Vanilla-Milk", D: "Double-Chocolate", N: "No-Glaze", S: "Sugar-Milk"};
 
     var cartItemsContainer = document.getElementsByClassName("cartItems");
-
-
     var productContainerDiv = document.createElement("div");
-
     productContainerDiv.className = "cartItem marginItem";
+
     // Product Image
     var productImg = document.createElement("img");
     productImg.className = "cartImage";
-    ///////// add src based on localStorage selected src
     productImg.src = "assets/images/noglaze.jpeg";
-    //////////////////////////////////////////////////////
 
-
-    // Item Details Div
     var itemDetailsDiv = document.createElement("div");
     itemDetailsDiv.className = "cartItemDetails";
 
@@ -207,10 +160,9 @@ function addProductToCart(item){
     quantityActualDropdownDiv.className = "dropdown";
     var quantityDropdownText = document.createElement("p");
     quantityDropdownText.className = "dropdownText";
-    // //////////////Change this based on items[1]
-    // console.log('quantityDropdownText.value');
+
+//This changes based on the selected quantity
     quantityDropdownText.innerHTML = itemQty;
-    // ///////////////////////////////////////////
     var quantityDropdownArrow = document.createElement("img");
     quantityDropdownArrow.className = "dropdownArrow";
     quantityDropdownArrow.src = "assets/icons/arrow.svg";
@@ -225,49 +177,65 @@ function addProductToCart(item){
     glazeActualDropdownDiv.className = "dropdown";
     var glazeDropdownText = document.createElement("p");
     glazeDropdownText.className = "dropdownText";
-    // /////////////////Change this based on items[0]
+
+//This changes based on the selected glaze
     glazeDropdownText.innerHTML = itemTypes[itemType];
-    ///////////////////////////////////////////
     var glazeDropdownArrow = document.createElement("img");
     glazeDropdownArrow.className = "dropdownArrow";
     glazeDropdownArrow.src = "assets/icons/arrow.svg";
 
+// Adding all elements inside their parents
+    glazeActualDropdownDiv.appendChild(glazeDropdownText);
+    glazeActualDropdownDiv.appendChild(glazeDropdownArrow);
+    glazeDropdownDiv.appendChild(glazeTitleText);
+    glazeDropdownDiv.appendChild(glazeActualDropdownDiv);
 
-glazeActualDropdownDiv.appendChild(glazeDropdownText);
-glazeActualDropdownDiv.appendChild(glazeDropdownArrow);
-glazeDropdownDiv.appendChild(glazeTitleText);
-glazeDropdownDiv.appendChild(glazeActualDropdownDiv);
+    quantityActualDropdownDiv.appendChild(quantityDropdownText);
+    quantityActualDropdownDiv.appendChild(quantityDropdownArrow);
+    quantityDropdownDiv.appendChild(quantityTitleText);
+    quantityDropdownDiv.appendChild(quantityActualDropdownDiv);
 
-quantityActualDropdownDiv.appendChild(quantityDropdownText);
-quantityActualDropdownDiv.appendChild(quantityDropdownArrow);
-quantityDropdownDiv.appendChild(quantityTitleText);
-quantityDropdownDiv.appendChild(quantityActualDropdownDiv);
-
-dropdownsDiv.appendChild(quantityDropdownDiv);
-dropdownsDiv.appendChild(glazeDropdownDiv);
-
-
-itemTitleDiv.appendChild(title);
-itemIconsDiv.appendChild(favoriteIcon);
-itemIconsDiv.appendChild(deleteIcon);
-itemTitleDiv.appendChild(itemIconsDiv);
-itemDetailsDiv.appendChild(itemTitleDiv);
+    dropdownsDiv.appendChild(quantityDropdownDiv);
+    dropdownsDiv.appendChild(glazeDropdownDiv);
 
 
-itemDetailsDiv.appendChild(productPrice);
-itemDetailsDiv.appendChild(dropdownsDiv);
+    itemTitleDiv.appendChild(title);
+    itemIconsDiv.appendChild(favoriteIcon);
+    itemIconsDiv.appendChild(deleteIcon);
+    itemTitleDiv.appendChild(itemIconsDiv);
+    itemDetailsDiv.appendChild(itemTitleDiv);
 
-productContainerDiv.appendChild(productImg);
-productContainerDiv.appendChild(itemDetailsDiv);
+
+    itemDetailsDiv.appendChild(productPrice);
+    itemDetailsDiv.appendChild(dropdownsDiv);
+
+    productContainerDiv.appendChild(productImg);
+    productContainerDiv.appendChild(itemDetailsDiv);
 
 
-
+// Adding the product listing to the empty cartDiv
     var cartItemsDiv = document.getElementById("cartDiv");
-    console.log(cartItemsDiv);
     cartItemsDiv.appendChild(productContainerDiv);
     console.log("New product added!");
 
+// delete a listing if the delete icon is clicked
+    deleteIcon.addEventListener('click', function(){
+        console.log('product was removed');
+        productContainerDiv.remove();
+        theSelection = item;
+        console.log(theSelection + " this is the selection");
+    });
+
 }
+
+
+
+
+
+
+
+
+
 
 
 
